b29acd82fac52940e855db6b00d7955545c917c4
Dekier <marcin.dekier97@gmail.com>
2019-09-27 18:55:06 +0200

    update title page

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: 'Deżal pewna jakość, niskie ceny, krótkie terminy w Poznaniu.',
+    title: 'Deżal: Rolety dzień - noc w Poznaniu jak i Rolety Rzymskie, Plisy. Gwarantowana dobra jakość.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
