47614e144854c8e95f4f0011d5730ac770766080
Marcin Dekier <marcin.dekier@appunite.com>
2021-06-09 17:51:37 +0200

    dfsdf

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,18 +97,15 @@ export default {
 
   firebase: {
     config: {
-      apiKey: 'AIzaSyAzEy6DqrnNLqqjLagqEnxmzYRMrJrYX8A',
-      authDomain: 'dezal-e34b9.firebaseapp.com',
-      databaseURL: 'https://dezal-e34b9.firebaseio.com',
-      projectId: 'dezal-e34b9',
-      storageBucket: 'dezal-e34b9.appspot.com',
-      messagingSenderId: '1098134824316',
-      appId: '1:1098134824316:web:03f74fc2d6433efa',
+      apiKey: "AIzaSyAzEy6DqrnNLqqjLagqEnxmzYRMrJrYX8A",
+      authDomain: "dezal-e34b9.firebaseapp.com",
+      databaseURL: "https://dezal-e34b9.firebaseio.com",
+      projectId: "dezal-e34b9",
+      storageBucket: "dezal-e34b9.appspot.com",
+      messagingSenderId: "1098134824316",
+      appId: "1:1098134824316:web:03f74fc2d6433efa"
     },
     services: {
-      auth: {
-        ssr: true,
-      },
       firestore: true
     },
   },
